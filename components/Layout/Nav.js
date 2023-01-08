8cb4d4ae00f04a36a8c40f82163702fac41d5abc
Sai Kumar Mudhiraj <68888124+Sayikumarr@users.noreply.github.com>
2023-01-08 11:21:06 +0530

    333

--- components/Layout/Nav.js
+++ components/Layout/Nav.js
@@ -13,9 +13,9 @@ const Nav = ()=>
     <>
        <header className="py-3 mb-4 border-bottom">
 			<div className="container d-flex flex-wrap justify-content-center">
-			  <a href="/" className="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
+			  <Link to="/" className="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
 				<svg className="bi me-2" width="40" height="20" ><img height="70" src="https://github.com/Sayikumarr/Sayikumarr/blob/main/logo.png?raw=true" alt="@TechBetainfo" /></svg>
-			  </a>
+			  </Link>
 			  <div className="col-12 col-lg-auto mb-3 mb-lg-0 ">
 	
 				<a className="btn btn-primary btn-lg btn-floating" style="background-color: #3b5998;" href="https://www.facebook.com/saikumar.mudhiraj.7262" role="button"
