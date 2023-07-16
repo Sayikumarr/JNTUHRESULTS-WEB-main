649c3fbcf15831c803ee9eb1b7612ab30d5e6a53
Sai Kumar Mudhiraj <68888124+Sayikumarr@users.noreply.github.com>
2023-07-16 14:53:36 +0530

    api endpoint

--- components/api/api.js
+++ components/api/api.js
@@ -1,5 +1,4 @@
 const url = "https://www.drsanjaysrirampur.com/jntuhapiserver";
-// const url = "http://127.0.0.1:8000";
 const urlr = "https://jntuhfastresultserver.herokuapp.com/";
 
 export default url;
